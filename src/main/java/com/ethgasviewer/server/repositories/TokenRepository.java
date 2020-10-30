6fb4386ab26f181f5b93e1a4f7488bbcebea7387
belbix <8683007@gmail.com>
2020-10-30 12:05:52 +0300

    aggregate history prices by hour

--- src/main/java/com/ethgasviewer/server/repositories/TokenRepository.java
+++ src/main/java/com/ethgasviewer/server/repositories/TokenRepository.java
@@ -9,7 +9,26 @@ import java.util.List;
 
 public interface TokenRepository extends JpaRepository<TokenEntity, Long> {
 
-    @Query("select t from TokenEntity t where t.nameId = :nameId and t.acquired > :fromDate")
+//    @Query("select t from TokenEntity t where t.nameId = :nameId and t.acquired > :fromDate")
+    @Query(nativeQuery = true, value = "" +
+        "select MAX(agg.id) id," +
+        " MAX(agg.name_id) name_id," +
+        " MAX(agg.acquired) acquired," +
+        " MAX(agg.price) price," +
+        " MAX(agg.volume) volume" +
+        " from ( " +
+        "select " +
+        "       t.id id, " +
+        "       t.name_id name_id, " +
+        "       t.acquired acquired, " +
+        "       t.price price, " +
+        "       t.volume volume, " +
+        "       DATE_FORMAT(FROM_UNIXTIME(t.acquired/1000), '%Y-%m-%d-%h') gtime " +
+        "from egv.token t " +
+        "where t.name_id = :nameId " +
+        "      and t.acquired > :fromDate " +
+        "    ) agg " +
+        "group by agg.gtime")
     List<TokenEntity> getFromDate(@Param("fromDate") long server, @Param("nameId") int nameId);
 
 }
