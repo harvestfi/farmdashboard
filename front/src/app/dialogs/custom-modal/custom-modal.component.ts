13ba06d0f8d986dd8973a5301bcd759fe47b7e54
brianetaveras <taveras574@gmail.com>
2021-01-22 14:40:01 -0500

    draggable window now works

--- front/src/app/dialogs/custom-modal/custom-modal.component.ts
+++ front/src/app/dialogs/custom-modal/custom-modal.component.ts
@@ -1,54 +1,73 @@
-import {AfterViewInit, Component, Renderer2, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-custom-modal',
   templateUrl: './custom-modal.component.html',
-  styleUrls: ['./custom-modal.component.css']
+  styleUrls: ['./custom-modal.component.css'],
+  host: {
+    '(document:mousemove)': 'mousemove($event)'
+  }
 })
 export class CustomModalComponent implements AfterViewInit {
-  private dragOffset;
   private isDown;
+  private mousePosition: { x: number, y: number };
+  private initialX: number;
+  private initialY: number;
+  private currentX: number;
+  private currentY: number;
+  private offsetX: number;
+  private offsetY: number;
   @ViewChild('modal') private modal;
 
   constructor(private renderer: Renderer2) {
     this.isDown = false;
-    this.dragOffset = [0, 0];
+    this.initialX;
+    this.initialY;
+    this.currentX;
+    this.currentY;
+    this.offsetX;
+    this.offsetY;
+    this.mousePosition = {
+      x: 0,
+      y: 0
+    }
   }
 
   ngAfterViewInit(): void {
+    const style: Record<any, any> = this.modal.nativeElement.style;
+    style.top = this.currentY + "px";
+    style.left = this.currentX + "px";
   }
 
   // todo fix drag and drop for APY window
   mousedown($event): void {
+    if ($event.stopPropagation) $event.stopPropagation();
+    this.offsetX = $event.layerY;
+    this.offsetY = $event.layerX;
     this.isDown = true;
-    const left = this.modal.nativeElement.offsetLeft - $event.clientX;
-    const top = this.modal.nativeElement.offsetTop - $event.clientY;
-    this.dragOffset = [
-      left,
-      top
-    ];
   }
-
   mouseup($event): void {
     this.isDown = false;
   }
 
   mousemove($event): void {
     $event.preventDefault();
+    const style: Record<any, any> = this.modal.nativeElement.style;
+    this.currentX = $event.clientX;
+    this.currentY = $event.clientY;
+    if ($event.target === this.modal.nativeElement) {
+      console.log(this.offsetY, this.offsetY)
+      if (this.isDown) {
 
-    if (this.isDown) {
-      const style = this.modal.nativeElement.style;
-      const left = ($event.clientX + this.dragOffset[0]) + 'px';
-      const top = ($event.clientY + this.dragOffset[1]) + 'px';
+        style.left = this.currentX + "px";
+        style.top = this.currentY + "px";
+        style.opacity = 0.6
 
+        style.transform = `translate(-50%, -10%)`
+        return;
+      }
 
-      console.log('old pos', style.left, style.top);
-      console.log('new positions', left, top);
-
-      style.left = left;
-      style.top = top;
-      // this.renderer.setStyle(this.modal.nativeElement, 'left', left);
-      // this.renderer.setStyle(this.modal.nativeElement, 'top', top);
+      style.opacity = 1
     }
   }
 
