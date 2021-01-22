6f367736a7a5c8f04212b28c980bf0a7f8624bf6
brianetaveras <taveras574@gmail.com>
2021-01-22 16:31:00 -0500

    Element moves when the screen is too small

--- front/src/app/dialogs/custom-modal/custom-modal.component.ts
+++ front/src/app/dialogs/custom-modal/custom-modal.component.ts
@@ -5,12 +5,12 @@ import { AfterViewInit, Component, Renderer2, ViewChild } from '@angular/core';
   templateUrl: './custom-modal.component.html',
   styleUrls: ['./custom-modal.component.css'],
   host: {
-    '(document:mousemove)': 'mousemove($event)'
+    '(document:mousemove)': 'mousemove($event)',
+    '(window:resize)': 'handleWindowResize($event)'
   }
 })
 export class CustomModalComponent implements AfterViewInit {
   private isDown;
-  private mousePosition: { x: number, y: number };
   private initialX: number;
   private initialY: number;
   private currentX: number;
@@ -27,10 +27,6 @@ export class CustomModalComponent implements AfterViewInit {
     this.currentY;
     this.offsetX;
     this.offsetY;
-    this.mousePosition = {
-      x: 0,
-      y: 0
-    }
   }
 
   ngAfterViewInit(): void {
@@ -39,37 +35,48 @@ export class CustomModalComponent implements AfterViewInit {
     style.left = this.currentX + "px";
   }
 
-  // todo fix drag and drop for APY window
   mousedown($event): void {
-    if ($event.stopPropagation) $event.stopPropagation();
-    this.offsetX = $event.layerY;
-    this.offsetY = $event.layerX;
-    this.isDown = true;
+    if ($event.target === this.modal.nativeElement) {
+      this.offsetX = $event.layerY;
+      this.offsetY = $event.layerX;
+      this.isDown = true;
+    }
   }
   mouseup($event): void {
     this.isDown = false;
   }
 
   mousemove($event): void {
-    $event.preventDefault();
     const style: Record<any, any> = this.modal.nativeElement.style;
     this.currentX = $event.clientX;
     this.currentY = $event.clientY;
-    if ($event.target === this.modal.nativeElement) {
-      console.log(this.offsetY, this.offsetY)
-      if (this.isDown) {
+    if (this.isDown) {
+      if ($event.preventDefault) $event.preventDefault();
+      if ($event.stopPropagation) $event.stopPropagation();
+      $event.cancelBubble = true;
 
-        style.left = this.currentX + "px";
-        style.top = this.currentY + "px";
-        style.opacity = 0.6
+      style.left = this.currentX + "px";
+      style.top = this.currentY + "px";
+      style.opacity = 0.6
 
-        style.transform = `translate(-50%, -10%)`
-        return;
-      }
-
-      style.opacity = 1
+      style.transform = `translate(-50%, -10%)`
+      return;
     }
+
+    style.opacity = 1
   }
 
+  handleWindowResize($event){
+    
+    const style: Record<any, any> = this.modal.nativeElement.style;
+    const windowWidth: number = $event.target.innerWidth 
+    const modalPosition: Record<any, any> = this.modal.nativeElement.getBoundingClientRect()
+    if (modalPosition.right > windowWidth){
+      style.left = "50%";
+    }
+
+  }
 }
+
+
 
