fd0a475efffbba14f0cafac13495bada877860cd
mrhatbirb <brianetaveras@gmail.com>
2021-01-24 15:03:35 -0500

    started main side menu refactor

---
+++ src/app/main/main-side-menu/main-side-menu.component.ts
@@ -0,0 +1,16 @@
+import {Component, AfterViewInit} from "@angular/core";
+
+@Component({
+    selector: "app-main-side-menu",
+    templateUrl: "./main-side-menu.component.html",
+    styleUrls: ["./main-side-menu.component.css"]
+})
+export class MainSideMenuComponent implements AfterViewInit {
+    constructor(){
+
+    }
+
+    ngAfterViewInit(): void {
+        // Do the thing :)
+    }
+}
