f31743153b7b8f0aa14c9779fb365c1a152aa002
mrhatbirb <brianetaveras@gmail.com>
2021-01-26 21:35:14 -0500

    Monst of original menu functionality re-added.

--- src/app/main/main-side-menu/main-side-menu.component.ts
+++ src/app/main/main-side-menu/main-side-menu.component.ts
@@ -1,16 +1,80 @@
-import {Component, AfterViewInit} from "@angular/core";
+import { Component, HostBinding } from "@angular/core";
+import { trigger, state, style, animate, transition } from "@angular/animations";
+import { AllStatsDialogComponent } from "../../dialogs/all-stats-dialog/all-stats-dialog.component";
+import { TvlDialogComponent } from '../../dialogs/tvl-dialog/tvl-dialog.component';
+import { RewardsDialogComponent } from '../../dialogs/rewards-dialog/rewards-dialog.component';
 
+import { MatDialog } from '@angular/material/dialog';
 @Component({
     selector: "app-main-side-menu",
     templateUrl: "./main-side-menu.component.html",
-    styleUrls: ["./main-side-menu.component.css"]
+    styleUrls: ["./main-side-menu.component.css"],
+    animations: [
+        trigger('openMenu', [
+            state('open', style({
+                left: "0%"
+            })),
+            state('closed', style({
+                left: "-50%"
+            })),
+            transition('open => closed', [
+                animate('0.5s')
+            ]),
+            transition('closed => open', [
+                animate('0.45s')
+            ]),
+        ]),
+    ]
 })
-export class MainSideMenuComponent implements AfterViewInit {
-    constructor(){
+export class MainSideMenuComponent {
+    showSideMenu = true;
+
+    constructor(private dialog: MatDialog) {
 
     }
 
-    ngAfterViewInit(): void {
-        // Do the thing :)
+    toggleMenu() {
+        this.showSideMenu = !this.showSideMenu;
+    }
+
+    openAllStats(): void {
+        this.dialog.open(AllStatsDialogComponent, {
+            data: {}
+        });
+        this.toggleMenu();
+    }
+
+    openTvlDialog(): void {
+        this.dialog.open(TvlDialogComponent, {
+            width: '100%',
+            height: 'auto',
+            data: {
+                type: 'All'
+            }
+        });
+        this.toggleMenu();
+    }
+
+    openIncomeDialog(): void {
+        this.dialog.open(TvlDialogComponent, {
+            width: '100%',
+            height: 'auto',
+            data: {
+                type: 'income'
+            }
+        });
+        this.toggleMenu();
+    }
+
+    openPsApyDialog(): void {
+        this.dialog.open(RewardsDialogComponent, {
+            width: '100%',
+            height: 'auto',
+            data: {
+                title: 'PS APY History',
+                name: ''
+            }
+        });
+        this.toggleMenu();
     }
 }
