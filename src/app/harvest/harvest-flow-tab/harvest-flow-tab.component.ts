bf65aceaee93a745b577f2190a615c9ee6823733
mrhatbirb <brianetaveras@gmail.com>
2021-01-29 12:22:25 -0500

    Some clenup. Removed calls not needed

--- src/app/harvest/harvest-flow-tab/harvest-flow-tab.component.ts
+++ src/app/harvest/harvest-flow-tab/harvest-flow-tab.component.ts
@@ -1,26 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HarvestDto } from '../../models/harvest-dto';
 import { ViewTypeService } from '../../services/view-type.service';
-import { StaticValues } from '../../static-values';
-import { Utils } from '../../utils';
 
 @Component({
   selector: 'app-harvest-flow-tab',
   templateUrl: './harvest-flow-tab.component.html',
   styleUrls: ['./harvest-flow-tab.component.css']
 })
-export class HarvestFlowTabComponent implements OnInit {
+export class HarvestFlowTabComponent{
 
   @Input() dtos: HarvestDto[] = [];
-  @Input() maxHeight = 800;
+  @Input() maxHeight = 400;
   @Input() minAmount = 0;
   @Input() vaultFilter = 'all';
   @Input() showFullDate = false;
 
   constructor(public vt: ViewTypeService) {
   }
-
-  ngOnInit(): void {
-  }
-
 }
