c81bd1438df3a63cd1705d9e2860ca4c016ca7f7
brianetaveras <taveras574@gmail.com>
2021-02-26 13:33:30 -0500

    Added color to spinner

--- src/app/main/loading-spinner/loading-spinner.component.ts
+++ src/app/main/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { ViewTypeService } from 'src/app/services/view-type.service';
 
 @Component({
   selector: 'app-loading-spinner',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoadingSpinnerComponent implements OnInit {
 
-  constructor() {
+  constructor(public vt: ViewTypeService) {
   }
 
   ngOnInit(): void {
