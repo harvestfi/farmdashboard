d76a07fa14adab8a08e61029dd61b4bba59d52d5
DarkSorrow <darksorrow.slade@gmail.com>
2021-03-07 13:43:24 +0300

    fix: Change fee to price history
    (cherry picked from commit f25f57cee42b4e5a604965025329ba3d561742d4)

--- src/app/dialogs/gas-dialog/gas-dialog.component.ts
+++ src/app/dialogs/gas-dialog/gas-dialog.component.ts
@@ -6,11 +6,6 @@ import {ChartBuilder} from '../../chart/chart-builder';
 import { ChartGeneralMethodsComponent } from 'src/app/chart/chart-general-methods.component';
 import { IChartApi } from 'lightweight-charts';
 
-interface FeeVaults {
-  fee: number;
-  vaults: number;
-}
-
 @Component({
   selector: 'app-gas-dialog',
   templateUrl: './gas-dialog.component.html',
@@ -34,29 +29,14 @@ export class GasDialogComponent extends ChartGeneralMethodsComponent implements 
 
   private loadData(): void {
     this.httpService.getLastTvls().subscribe(data => {
-      this.log.debug('Gas fees history ', data);
+      this.log.debug('Gas price history ', data);
       const chartBuilder = new ChartBuilder();
-      chartBuilder.initVariables(2);
-      const dates = [];
-      const dateData : Record<string, FeeVaults> = {};
-      data?.forEach(dto => {
-        if (dateData[dto.blockDate] === undefined) {
-          dates.push(dto.blockDate); // use to sort if necessary
-          dateData[dto.blockDate] = {
-            fee: 0.0,
-            vaults: 0,
-          };
-        }
-        dateData[dto.blockDate].fee += dto.lastGas;
-        dateData[dto.blockDate].vaults++;
+      chartBuilder.initVariables(1);
+      data?.sort((a, b) => (a.blockDate > b.blockDate) ? 1 : -1).forEach(dto => {
+          chartBuilder.addInData(0, dto.blockDate, dto.lastGas);
       });
-      for (const date of dates.sort()) {
-        chartBuilder.addInData(0, date, dateData[date].fee/* / dateData[date].vaults*/);
-        chartBuilder.addInData(1, date, dateData[date].vaults);
-      }
       this.handleData(chartBuilder, [
-        ['Fees', 'right', '#0085ff'],
-        ['Vaults', '1', '#7e7e7e']
+        ['Gas price', 'right', '#0085ff'],
       ]);
     });
   }
