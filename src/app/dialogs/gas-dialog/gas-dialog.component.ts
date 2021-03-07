ae94ca60d0e8122fe94cfb65ec75700c64b7972f
DarkSorrow <darksorrow.slade@gmail.com>
2021-03-07 19:59:51 +0100

    fix: Change to getHarvestTxHistoryByRange for gas price history
    (cherry picked from commit bcb4356bb8975dcce9f8b31a1d7a9ea23bf3a51a)

--- src/app/dialogs/gas-dialog/gas-dialog.component.ts
+++ src/app/dialogs/gas-dialog/gas-dialog.component.ts
@@ -1,10 +1,10 @@
 import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, Input, ViewChild} from '@angular/core';
+import {StaticValues} from '../../static/static-values';
 import {HttpService} from '../../services/http.service';
 import {ViewTypeService} from '../../services/view-type.service';
 import {NGXLogger} from 'ngx-logger';
 import {ChartBuilder} from '../../chart/chart-builder';
 import { ChartGeneralMethodsComponent } from 'src/app/chart/chart-general-methods.component';
-import { IChartApi } from 'lightweight-charts';
 
 @Component({
   selector: 'app-gas-dialog',
@@ -28,12 +28,17 @@ export class GasDialogComponent extends ChartGeneralMethodsComponent implements 
   }
 
   private loadData(): void {
-    this.httpService.getLastTvls().subscribe(data => {
+    const currentDate = Math.ceil(new Date().getTime() / 1000);
+    this.httpService.getHarvestTxHistoryByRange(currentDate - StaticValues.SECONDS_OF_MONTH, currentDate).subscribe(data => {
       this.log.debug('Gas price history ', data);
       const chartBuilder = new ChartBuilder();
+      let prevDate = 0;
       chartBuilder.initVariables(1);
-      data?.sort((a, b) => (a.blockDate > b.blockDate) ? 1 : -1).forEach(dto => {
+      data?.forEach(dto => {
+        if (prevDate !== dto.blockDate) {
           chartBuilder.addInData(0, dto.blockDate, dto.lastGas);
+        }
+        prevDate = dto.blockDate;
       });
       this.handleData(chartBuilder, [
         ['Gas price', 'right', '#0085ff'],
