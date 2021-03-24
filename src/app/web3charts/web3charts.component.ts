4c8c198c986457002f1e2fa145861b8d6635219d
const_malytin <const.malytin@gmail.com>
2021-03-24 22:26:25 +0300

    fix

--- src/app/web3charts/web3charts.component.ts
+++ src/app/web3charts/web3charts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core'
+import { Component, AfterViewInit, ViewChild, ElementRef, ChangeDetectorRef, Input } from '@angular/core'
 import { Web3Service } from '../services/web3.service'
 import { times, ethblocksperhour, ethblocksperday } from '../statistic-board/Abi'
 import { ChartGeneralMethodsComponent } from 'src/app/chart/chart-general-methods.component';
@@ -6,138 +6,170 @@ import { ViewTypeService } from '../services/view-type.service'
 import { ChartBuilder } from '../chart/chart-builder'
 import {IChartApi} from 'lightweight-charts';
 
+// - - - -
+import {HttpService} from '../services/http.service';
+
+
 import type { Period } from '../statistic-board/Abi'
 @Component({
   selector: 'app-web3charts',
   templateUrl: './web3charts.component.html',
   styleUrls: ['./web3charts.component.css']
 })
-export class Web3chartsComponent extends ChartGeneralMethodsComponent implements OnInit { // AfterViewInit
-  @ViewChild('chartShares') chartSharesRef: ElementRef;
-  @ViewChild('chartTvl') chartTvlRef: ElementRef;
-  chartShares: IChartApi;
-  chartTvl: IChartApi;
-
-  selectedPeriod: Period = times[3]
-  preriodValues: Period[] = times
+export class Web3chartsComponent extends ChartGeneralMethodsComponent implements AfterViewInit { // AfterViewInit
   
-  isLoading = true
-  
-  contracts = []
-  selectedContractId = null
+  @ViewChild('chart') chartEl: ElementRef;
+@Input() public data: Record<any, any>;
+ready = false;
+chart: IChartApi;
+constructor(private httpService: HttpService,
+            public vt: ViewTypeService,
+            private cdRef: ChangeDetectorRef) {
+              super();
+}
+ngAfterViewInit(): void {
+  this.loadData();
+}
+private loadData(): void {
+  this.httpService.getHardWorkHistoryData().subscribe(data => {
+    console.log('- - - - data', data)
+    const chartBuilder = new ChartBuilder();
+    chartBuilder.initVariables(1);
+    data?.forEach(dto => chartBuilder.addInData(0, dto.blockDate, (dto.weeklyAllProfit / 0.7) / 1000));
+    // data?.forEach(dto => chartBuilder.addInData(1, dto.blockDate, dto.tvl / 1000000));
+    this.handleData(chartBuilder, [
+      ['Weekly Profit K$', 'right', '#0085ff'],
+      // ['TVL M$', '1', '#7e7e7e']
+    ]);
+  });
+}
+private handleData(chartBuilder: ChartBuilder, config: string[][]): void {
+  this.ready = true;
+  this.cdRef.detectChanges();
+  this.chart = chartBuilder.initChart(this.chartEl);
+  chartBuilder.addToChart(this.chart, config);
+}
+  // @ViewChild('chartShares') chartSharesRef: ElementRef;
+  // @ViewChild('chartTvl') chartTvlRef: ElementRef;
+  // chartShares: IChartApi;
+  // chartTvl: IChartApi;
 
-  chartsData = {
-    shares: null,
-    tvl: null,
-  }
+  // selectedPeriod: Period = times[2]
+  // preriodValues: Period[] = times
+  // 
+  // isLoading = true
+  // 
+  // contracts = []
+  // selectedContractId = null
 
-  constructor(
-    public vt: ViewTypeService,
-    private web3: Web3Service,
-    private cdRef: ChangeDetectorRef,
-  ) {
-    super();
-  }
+  // chartsData = {
+    // shares: null,
+    // tvl: null,
+  // }
 
-  async ngOnInit() {
-    await this.web3.init()
-    this.getContractList()
-    this.selectedContractId = this.contracts[6].id
-    await this.getChartData()
-    this.renderCharts();
-  }
+  // constructor(
+    // public vt: ViewTypeService,
+    // private web3: Web3Service,
+    // private cdRef: ChangeDetectorRef,
+  // ) {
+    // super();
+  // }
 
-  getContractList() {
-    this.contracts = this.web3.constracts.map((el) => {
-      return {
-        id: el.contract.id,
-        name: el.contract.name,
-      }
-    })
-  }
+  // async ngAfterViewInit() {
+    // await this.web3.init()
+    // this.getContractList()
+    // this.selectedContractId = this.contracts[14].id
+    // await this.getChartData()
+    // this.renderCharts();
+  // }
 
-  async selectContract(contractId) {
-    this.selectedContractId = Number(contractId)
-    await this.getChartData()
-  }
+  // getContractList() {
+    // this.contracts = this.web3.constracts.map((el) => {
+      // return {
+        // id: el.contract.id,
+        // name: el.contract.name,
+      // }
+    // })
+  // }
 
-  async selectPeriod(period: Period) {
-    this.selectedPeriod = period
-    await this.getChartData()
-  }
+  // async selectContract(contractId) {
+    // this.selectedContractId = Number(contractId)
+    // await this.getChartData()
+  // }
 
-  showLoader() {
-    this.isLoading = true
-  }
+  // async selectPeriod(period: Period) {
+    // this.selectedPeriod = period
+    // await this.getChartData()
+  // }
 
-  hideLoader() {
-    this.isLoading = false
-  }
+  // showLoader() {
+    // this.isLoading = true
+  // }
 
-  renderCharts() {
-    this.handleData(
-      this.chartsData.shares,
-      [['Shares', 'right', '#7e7e7e']],
-      this.chartSharesRef,
-      this.chartShares,
-    );
+  // hideLoader() {
+    // this.isLoading = false
+  // }
 
-    this.handleData(
-      this.chartsData.tvl,
-      [['TVL ', 'right', '#7e7e7e']],
-      this.chartTvlRef,
-      this.chartTvl,
-    );
-  }
+  // renderCharts() {
+    // this.handleData(
+      // this.chartsData.shares,
+      // [['Shares', 'right', '#7e7e7e']],
+      // this.chartSharesRef,
+      // this.chartShares,
+    // );
 
-  private handleData(data: any, config: string[][], elRef: ElementRef, chart: IChartApi): void {
-    if (!data) {
-      return
-    }
-    
-    const builder = new ChartBuilder();
-    builder.initVariables(1);
+    // this.handleData(
+      // this.chartsData.tvl,
+      // [['TVL ', 'right', '#7e7e7e']],
+      // this.chartTvlRef,
+      // this.chartTvl,
+    // );
+  // }
 
-    data.forEach(item => {
-      builder.addInData(0, item.timestamp, item.share);
-    })
-    this.cdRef.detectChanges();
-    chart = builder.initChart(elRef)
-    builder.addToChart(chart, config)
-  }
+  // private handleData(data: any, config: string[][], elRef: ElementRef, chart: IChartApi): void {
+    // if (!data) {
+      // return
+    // }
+    // 
+    // const chartBuilder = new ChartBuilder();
+    // chartBuilder.initVariables(1);
+    // data.forEach(el => {
+      // console.log('el', el)
+      // chartBuilder.addInData(0, el.timestamp, el.share)
+    // })
 
-  async getChartData() {
-    this.showLoader()
+    // this.cdRef.detectChanges();
+    // this.chartShares = chartBuilder.initChart(this.chartSharesRef);
+    // chartBuilder.addToChart(this.chartShares, config);
+  // }
 
-    const blocksPeriod = this.selectedPeriod.value === 24 ? ethblocksperhour : ethblocksperday
-    
-    const pShares = this.web3.getPrice(
-      this.selectedContractId,
-      this.selectedPeriod.value,
-      blocksPeriod,
-      'shares'
-    )
-    
-    const pTvl = this.web3.getPrice(
-      this.selectedContractId,
-      this.selectedPeriod.value,
-      blocksPeriod,
-      'tvl'
-    )
+  // async getChartData() {
+    // this.showLoader()
+
+    // const blocksPeriod = this.selectedPeriod.value === 24 ? ethblocksperhour : ethblocksperday
+    // 
+    // const pShares = this.web3.getPrice(
+      // this.selectedContractId,
+      // this.selectedPeriod.value,
+      // blocksPeriod,
+      // 'shares'
+    // )
+    // 
+    // const pTvl = this.web3.getPrice(
+      // this.selectedContractId,
+      // this.selectedPeriod.value,
+      // blocksPeriod,
+      // 'tvl'
+    // )
 
     // @ts-ignore
-    const [{ value: responseShares }, { value: responsepTvl }] = await Promise.allSettled([pShares, pTvl])
+    // const [{ value: responseShares }, { value: responsepTvl }] = await Promise.allSettled([pShares, pTvl])
 
-    this.chartsData = {
-      shares: responseShares,
-      tvl: responsepTvl,
-    }
+    // this.chartsData = {
+      // shares: responseShares,
+      // tvl: responsepTvl,
+    // }
 
-    console.log('- - - - response', {
-      shares: responseShares,
-      tvl: responsepTvl,
-    })
-
-    this.hideLoader()
-  }
+    // this.hideLoader()
+  // }
 }
