ae6c6c7ded5f602a7bd3c2e827be5a03ffd6c61b
belbix <7453635@gmail.com>
2021-04-04 13:58:46 +0300

    remove path webpack

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   wsEndpoint: 'https://ethparser.herokuapp.com/stomp',
   apiEndpoint: 'https://ethparser-api.herokuapp.com',
   wsReconnectInterval: 15,
-  debugLevel: 3
+  debugLevel: 3,
+  web3Url: 'wss://eth-mainnet.ws.alchemyapi.io/v2/nzZTij_2KAavafMTicQTL52SkxJf1Lkz',
 };
