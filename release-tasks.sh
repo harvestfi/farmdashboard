622da656f1894fb7c912f840069c34d370ec3404
Harvester <81636149+TheHarvesterPM@users.noreply.github.com>
2021-04-05 16:10:15 -0600

    check working dir

--- release-tasks.sh
+++ release-tasks.sh
@@ -5,10 +5,14 @@ if [[ -z $DEPLOYMENT_ENV ]]; then
     echo "DEPLOYMENT_ENV is not set"
 else
     if [[ $DEPLOYMENT_ENV == "prod" ]]; then 
+        pwd
+        cat dist/farmdashboard-front/assets/config.prod.json
         cp dist/farmdashboard-front/assets/config.prod.json dist/farmdashboard-front/assets/config.json
     fi
 
     if [[ $DEPLOYMENT_ENV == "stage" ]]; then 
+        pwd
+        dist/farmdashboard-front/assets/config.stage.json
         cp dist/farmdashboard-front/assets/config.stage.json dist/farmdashboard-front/assets/config.json
     fi
 fi
