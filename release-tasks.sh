5f1eddf92f866287eeab5954f55cc8be39e582b4
Harvester <81636149+TheHarvesterPM@users.noreply.github.com>
2021-04-05 13:26:05 -0600

    fix conditional

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,12 @@
-if ["$DEPLOYMENT_ENV" == "prod"]
-then 
-    mv dist/farmdashboard-front/assets/config.prod.json dist/farmdashboard-front/assets/config.json
-fi
+echo DEPLOYMENT_ENV = $DEPLOYMENT_ENV
+if [[ -z $DEPLOYMENT_ENV ]]; then
+    echo "1"
+else
+    if [[ $DEPLOYMENT_ENV == "prod" ]]; then 
+        cp dist/farmdashboard-front/assets/config.prod.json dist/farmdashboard-front/assets/config.json
+    fi
 
-if ["$DEPLOYMENT_ENV" == "stage"]
-then 
-    mv dist/farmdashboard-front/assets/config.stage.json dist/farmdashboard-front/assets/config.json
+    if [[ $DEPLOYMENT_ENV == "stage" ]]; then 
+        cp dist/farmdashboard-front/assets/config.stage.json dist/farmdashboard-front/assets/config.json
+    fi
 fi
