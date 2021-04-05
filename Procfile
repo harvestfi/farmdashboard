2fa53020832063b4b28038fb1d702e490fda0a5a
Harvester <81636149+TheHarvesterPM@users.noreply.github.com>
2021-04-05 08:07:20 -0600

    Revert "setup release"
    This reverts commit f2db009a7097dad9c64783901010991a68ec0217.

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: ./release-tasks.sh
-web: npm start
--- release-tasks.sh
+++
@@ -1,2 +0,0 @@
-rm dist/farmdashboard-front/assets/config.json
-mv dist/farmdashboard-front/assets/config.prod.json dist/farmdashboard-front/assets/config.json
