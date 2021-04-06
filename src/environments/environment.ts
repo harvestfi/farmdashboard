1ca6eeea5e29cc3c7c7965fa09326ed93b9a1b8f
Harvester <81636149+TheHarvesterPM@users.noreply.github.com>
2021-04-06 15:51:36 -0600

    wording

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // 1) make changes to src/assets/config.[environment].json
 // 2) update src/app.config.ts
 // 3) add constructor(@Inject(APP_CONFIG) public config: AppConfig) to your class
-// 4) access config via this.config.apiEndpoint
+// 4) access config via this.config.variable example this.config.apiEndpoint
 
 export const environment = {
   production: false,
