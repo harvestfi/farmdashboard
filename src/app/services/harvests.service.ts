a80d48675a3f941ae4e61533fec7888323e5891d
James Turner <james.turner01@trinitymirror.com>
2021-04-12 20:57:57 +0100

    Harvests was missing network stuff

--- src/app/services/harvests.service.ts
+++ src/app/services/harvests.service.ts
@@ -5,6 +5,8 @@ import {Inject, Injectable} from '@angular/core';
 import {APP_CONFIG, AppConfig} from '../../app.config';
 import {HttpClient} from '@angular/common/http';
 import {SnackService} from './snack.service';
+import {Network} from '../models/network';
+import {StaticValues} from '../static/static-values';
 
 @Injectable({
     providedIn: 'root'
@@ -19,31 +21,31 @@ export class HarvestsService {
         console.log('apiEndpoint is: ' + this.apiEndpoint);
     }
 
-    getHarvestTxHistoryByRange(minBlock: number, maxBlock: number): Observable<HarvestDto[]> {
+    getHarvestTxHistoryByRange(minBlock: number, maxBlock: number, network: Network  = StaticValues.NETWORK_ETH): Observable<HarvestDto[]> {
         return this.http.get<HarvestDto[]>(this.apiEndpoint + `${this.url}/history/harvest?from=${minBlock}&to=${maxBlock}`).pipe(
             catchError(this.snackService.handleError<HarvestDto[]>(`Harvest history`))
         );
     }
 
-    getHarvestHistoryByVault(name: string): Observable<HarvestDto[]> {
+    getHarvestHistoryByVault(name: string, network: Network  = StaticValues.NETWORK_ETH): Observable<HarvestDto[]> {
         return this.http.get<HarvestDto[]>(this.apiEndpoint + `${this.url}/history/harvest/` + name).pipe(
             catchError(this.snackService.handleError<HarvestDto[]>(name + `Harvest history`))
         );
     }
 
-    getLastTvls(): Observable<HarvestDto[]> {
+    getLastTvls(network: Network  = StaticValues.NETWORK_ETH): Observable<HarvestDto[]> {
         return this.http.get<HarvestDto[]>(this.apiEndpoint + '/api/transactions/last/harvest').pipe(
             catchError(this.snackService.handleError<HarvestDto[]>(`last TVL `))
         );
     }
 
-    getAddressHistoryHarvest(address: string): Observable<HarvestDto[]> {
+    getAddressHistoryHarvest(address: string, network: Network  = StaticValues.NETWORK_ETH): Observable<HarvestDto[]> {
         return this.http.get<HarvestDto[]>(this.apiEndpoint + '/history/harvest/' + address).pipe(
             catchError(this.snackService.handleError<HarvestDto[]>(`history address harvest `))
         );
     }
 
-    getHarvestTxHistoryData(): Observable<HarvestDto[]> {
+    getHarvestTxHistoryData(network: Network  = StaticValues.NETWORK_ETH): Observable<HarvestDto[]> {
         return this.http.get<HarvestDto[]>(this.apiEndpoint + `${this.url}/history/harvest`).pipe(
             catchError(this.snackService.handleError<HarvestDto[]>(`Harvest history`))
         );
