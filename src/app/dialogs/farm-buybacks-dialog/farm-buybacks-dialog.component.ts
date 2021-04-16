11f853acd0f9bc1b39ed80529dcb34840a9b7c8b
belbix <7453635@gmail.com>
2021-04-16 14:43:41 +0300

    fix bsc farm buyback chart

--- src/app/dialogs/farm-buybacks-dialog/farm-buybacks-dialog.component.ts
+++ src/app/dialogs/farm-buybacks-dialog/farm-buybacks-dialog.component.ts
@@ -1,12 +1,13 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, Input, ViewChild} from '@angular/core';
-import {HttpService} from '../../services/http/http.service';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import {ViewTypeService} from '../../services/view-type.service';
 import {NGXLogger} from 'ngx-logger';
 import {ChartBuilder} from '../../chart/chart-builder';
 import {ChartGeneralMethodsComponent} from '../../chart/chart-general-methods.component';
-import { IChartApi } from 'lightweight-charts';
+import {IChartApi} from 'lightweight-charts';
 import {HardworksService} from '../../services/http/hardworks.service';
 import {TvlsService} from '../../services/http/tvls.service';
+import {PricesCalculationService} from '../../services/prices-calculation.service';
+
 @Component({
   selector: 'app-farm-buybacks-dialog',
   templateUrl: './farm-buybacks-dialog.component.html',
@@ -23,8 +24,9 @@ export class FarmBuybacksDialogComponent extends ChartGeneralMethodsComponent im
               private cdRef: ChangeDetectorRef,
               private log: NGXLogger,
               private hardworksService: HardworksService,
+              private priceService: PricesCalculationService,
               private tvlsService: TvlsService,
-              ) {
+  ) {
                 super();
   }
 
@@ -37,7 +39,13 @@ export class FarmBuybacksDialogComponent extends ChartGeneralMethodsComponent im
       this.log.debug('History of All Farm buybacks loaded ', data);
       const chartBuilder = new ChartBuilder();
       chartBuilder.initVariables(2);
-      data?.forEach(dto => chartBuilder.addInData(0, dto.blockDate, dto.farmBuybackSum / 1000));
+      data?.forEach(dto => {
+        let bb = dto.farmBuybackSum / 1000;
+        if (dto.network === 'bsc') {
+          bb = bb / this.priceService.lastFarmPrice();
+        }
+        chartBuilder.addInData(0, dto.blockDate, bb);
+      });
 
       this.tvlsService.getHistoryTvlByVault('PS').subscribe(vaultData => {
             this.log.debug('History of PS TVL loaded ', vaultData);
