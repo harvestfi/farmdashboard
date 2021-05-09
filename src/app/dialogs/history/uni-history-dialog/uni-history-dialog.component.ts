fe6e6b63591c5d1c207e63e33354a41b2cf6207f
brianetaveras <taveras574@gmail.com>
2021-05-09 15:25:51 -0400

    Paginated uniswap trades modal

--- src/app/dialogs/history/uni-history-dialog/uni-history-dialog.component.ts
+++ src/app/dialogs/history/uni-history-dialog/uni-history-dialog.component.ts
@@ -5,6 +5,7 @@ import { ViewTypeService } from '../../../services/view-type.service';
 import { NGXLogger } from 'ngx-logger';
 import { UniswapDto } from '../../../models/uniswap-dto';
 import {UniswapService} from '../../../services/http/uniswap.service';
+import { Paginated } from 'src/app/models/paginated';
 
 
 @Component({
@@ -14,9 +15,17 @@ import {UniswapService} from '../../../services/http/uniswap.service';
 })
 export class UniHistoryDialogComponent implements AfterViewInit {
   dtos: UniswapDto[] = [];
+  paginated_dtos: Paginated<UniswapDto> = {
+    currentPage: 0,
+    nextPage: -1,
+    previousPage: -1,
+    totalPages: 0,
+    data: []
+};
   txIds = new Set<string>();
   lowestBlockDate = 999999999999;
   disabled = false;
+  minAmount = 0;
 
 
   constructor(
@@ -27,8 +36,14 @@ export class UniHistoryDialogComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-
-    this.txHistory.getUniswapTxHistoryData().subscribe(data => this.addInArray(data));
+    // TODO: This needs to use the actual endpoint :)
+    this.txHistory.getUniswapPaginatedTxHistoryData().then(data =>{
+      const d = data.data;
+      this.paginated_dtos = data;
+      this.paginated_dtos.data = [];
+      this.addInPaginatedArray(d);
+    });
+    // this.txHistory.getUniswapTxHistoryData().subscribe(data => this.addInArray(data));
   }
 
   getOlderTransactions(): void {
@@ -52,6 +67,23 @@ export class UniHistoryDialogComponent implements AfterViewInit {
     return true;
   }
 
+  private addInPaginatedArray(newValues: UniswapDto[]): void {
+    newValues.forEach((tx: UniswapDto) => {
+      if(tx.type === 'ADD' || tx.type === 'REM'){
+        return;
+      }
+      if(!this.isUniqTx(tx)){
+        this.log.warn('Not unique transaction', tx);
+        return;
+      }
+      if (tx.blockDate < this.lowestBlockDate) {
+        this.lowestBlockDate = tx.blockDate;
+      }
+      UniswapDto.round(tx);
+      this.paginated_dtos.data.push(tx);
+    });
+  }
+
   private addInArray(newValues: UniswapDto[]): void {
 
     this.log.info('New values', newValues);
@@ -71,5 +103,32 @@ export class UniHistoryDialogComponent implements AfterViewInit {
       this.dtos.push(tx);
 
     }
+  }
+
+  getUniDataForPage(page_number): void {
+    this.txIds = new Set();
+    this.txHistory.getUniswapPaginatedTxHistoryData(page_number, 10, this.minAmount)
+    .then(data => {
+      const d = data.data;
+      this.paginated_dtos = data;
+      this.paginated_dtos.data = [];
+      console.log(d);
+      this.addInPaginatedArray(d);
+    });
+  }
+
+  nextPage($event): void {
+    this.getUniDataForPage($event);
+  }
+  previousPage($event): void {
+    this.getUniDataForPage($event);
+  }
+  selectPage($event): void {
+    this.getUniDataForPage($event);
+
+  }
+
+  handleFilterUpdate(_$event): void {
+    this.getUniDataForPage(0);
   }
 }
--- src/app/services/http/uniswap.service.ts
+++ src/app/services/http/uniswap.service.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {UniswapDto} from '../../models/uniswap-dto';
 import {HttpService} from './http.service';
 import {OhlcDto} from '../../models/ohlc-dto';
+import { Paginated } from 'src/app/models/paginated';
 
 @Injectable({
     providedIn: 'root'
@@ -21,6 +22,24 @@ export class UniswapService {
 
     getUniswapTxHistoryData(): Observable<UniswapDto[]> {
         return this.httpService.httpGet('/api/transactions/history/uni');
+    }
+
+    getUniswapPaginatedTxHistoryData(
+        page_number: number = 1,
+        page_size: number = 10,
+        min_amount: number = 0,
+        ordering: string = 'desc'
+    ): Promise<any> {
+        // eslint-disable-next-line max-len
+        return fetch(`http://localhost:3000/uni?_page=${page_number}&_limit=${page_size}&_sort=amount&_order=${ordering}?amount_gte=${min_amount}`)
+        .then(r => r.json())
+        .then((data) => ({
+                currentPage: page_number,
+                nextPage: page_number + 1 > 10 ? -1 : page_number + 1,
+                previousPage: page_number -1,
+                totalPages: 10,
+                data
+            }));
     }
 
     getUniswapTxHistoryByRange(minBlock: number, maxBlock: number): Observable<UniswapDto[]> {
