d3187680066d442aec0e41d4e6a0f963fd8cc9ec
brianetaveras <taveras574@gmail.com>
2021-05-25 20:52:46 -0400

    added styling to uni trade

--- src/app/flow-cards/uniswap/uni-tx/uni-tx.component.ts
+++ src/app/flow-cards/uniswap/uni-tx/uni-tx.component.ts
@@ -11,7 +11,7 @@ import {UniswapDataService} from '../../../services/data/uniswap-data.service';
 })
 export class UniTxComponent implements AfterViewInit {
   @ViewChild('unitHistoryModal') private unitHistoryModal: CustomModalComponent;
-
+  minAmount = 0;
   constructor(
       private uniswapData: UniswapDataService,
       public vt: ViewTypeService,
@@ -27,6 +27,19 @@ export class UniTxComponent implements AfterViewInit {
 
   openUniHistory(): void {
     this.unitHistoryModal.open();
+  }
+
+  checkImportantOwner(dto: UniswapDto): string {
+    const address = dto.owner;
+    if (address.toLowerCase() === '0xbed04c43e74150794f2ff5b62b4f73820edaf661'.toLowerCase()
+        || address.toLowerCase() === '0xed1eac72063476e04997fbefa19dcaea008e2aa5'.toLowerCase()
+        || dto.methodName === 'doHardWork') {
+      return 'doHardWork';
+    }
+    if (address === '0x843002b1d545ef7abb71c716e6179570582faa40' || address === '0x49d71131396f23f0bce31de80526d7c025981c4d') {
+      return 'devs';
+    }
+    return 'normal';
   }
 
 }
