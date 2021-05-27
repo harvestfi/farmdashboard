90e1815d27bddaf6ed7edc6c2d1d21d42dc99dea
belbix <7453635@gmail.com>
2021-05-27 13:06:50 +0300

    polish a bit difference between ethparser blocks and actual

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,11 +2,10 @@ import {InjectionToken} from '@angular/core';
 
 export class AppConfig {
     debugLevel: number;
-    infuraKey: string;
     apiEndpoints = {};
     wsEndpoints = {};
     wsReconnectInterval: number;
-    web3Url: string;
+    ethRpcUrl: string;
     multipleSources: boolean;
     defaultNetwork: string;
 }
--- src/app/static/networks.ts
+++ src/app/static/networks.ts
@@ -1,36 +1,35 @@
 import {Network} from '../models/network';
-import {Addresses} from './addresses';
 import {APP_CONFIG} from '../../app.config';
 import {Inject, Injectable} from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class Networks {
 
-    private makeEthNetwork = (key): Network => ({
-        blockExplorerUrl: 'https://etherscan.io',
-        chainId: 1,
-        ethparserName: 'eth',
-        currencySymbol: 'ETH',
-        name: 'Ethereum Mainnet',
-        rpcUrl: 'https://mainnet.infura.io/v3/'+key,
-    });
-    private static NETWORK_BSC: Network = {
-        blockExplorerUrl: 'https://www.bscscan.com',
-        chainId: 56,
-        ethparserName: 'bsc',
-        currencySymbol: 'BNB',
-        name: 'Binance Smart Chain',
-        rpcUrl: 'https://bsc-dataseed.binance.org/',
-    };
-    public NETWORKS: Map<string, Network> = new Map<string, Network>([
-        ['bsc', Networks.NETWORK_BSC]
-    ]);
+  private makeEthNetwork = (rpcUrl: string): Network => ({
+    blockExplorerUrl: 'https://etherscan.io',
+    chainId: 1,
+    ethparserName: 'eth',
+    currencySymbol: 'ETH',
+    name: 'Ethereum Mainnet',
+    rpcUrl: '' + rpcUrl,
+  });
+  private static NETWORK_BSC: Network = {
+    blockExplorerUrl: 'https://www.bscscan.com',
+    chainId: 56,
+    ethparserName: 'bsc',
+    currencySymbol: 'BNB',
+    name: 'Binance Smart Chain',
+    rpcUrl: 'https://bsc-dataseed.binance.org/',
+  };
+  public NETWORKS: Map<string, Network> = new Map<string, Network>([
+    ['bsc', Networks.NETWORK_BSC]
+  ]);
 
-    constructor(@Inject(APP_CONFIG) config) {
-        this.NETWORKS.set('eth', this.makeEthNetwork(config.infuraKey));
-    }
+  constructor(@Inject(APP_CONFIG) config) {
+    this.NETWORKS.set('eth', this.makeEthNetwork(config.ethRpcUrl));
+  }
 
 
 }
