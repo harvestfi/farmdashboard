e25fd56042b8c8df70022bc031d8e011906aa8a9
Ryan <rdturner40@gmail.com>
2021-05-29 10:35:23 -0400

    selected correct rewards metric, added comparison between buy back and rewards

--- src/app/dialogs/charts/farm-buybacks-dialog/farm-buybacks-dialog.component.ts
+++ src/app/dialogs/charts/farm-buybacks-dialog/farm-buybacks-dialog.component.ts
@@ -6,6 +6,7 @@ import {ChartGeneralMethodsComponent} from '../../../chart/chart-general-methods
 import {HardworksService} from '../../../services/http/hardworks.service';
 import {TvlsService} from '../../../services/http/tvls.service';
 import {PriceDataService} from '../../../services/data/price-data.service';
+import {RewardsService} from '../../../services/http/rewards.service'
 import {StaticValues} from '../../../static/static-values';
 import {Addresses} from '../../../static/addresses';
 import {RewardDataService} from '../../../services/data/reward-data.service';
@@ -24,21 +25,9 @@ export class FarmBuybacksDialogComponent extends ChartGeneralMethodsComponent im
               private hardworksService: HardworksService,
               private priceData: PriceDataService,
               private tvlsService: TvlsService,
-              private rewardService: RewardDataService,
+              private rewardService: RewardsService,
   ) {
     super(cdRef, vt);
-  }
-
-  private calcRewardComparison(address: string, network: string, buyBack: number): number {
-    // Calculate the ratio between the buyback and reward for a given block.
-    const reward =  this.rewardService.getReward(address, network);
-    if (buyBack !== 0 || reward !== 0){
-      if(buyBack > reward){
-        return buyBack / reward;
-      } else {
-        return reward / buyBack;
-      }
-    }
   }
 
   load(): void {
@@ -46,14 +35,24 @@ export class FarmBuybacksDialogComponent extends ChartGeneralMethodsComponent im
     forkJoin([
       this.hardworksService.getHardWorkHistoryData(StaticValues.NETWORKS.get(this.network)),
       this.tvlsService.getHistoryTvlByVault(Addresses.ADDRESSES.get('PS')),
-    ]).subscribe(([hardWorks, vaultData, ]) => {
+      this.rewardService.getHistoryRewards(Addresses.ADDRESSES.get('PS'), StaticValues.NETWORKS.get('eth')),
+    ]).subscribe(([hardWorks, vaultData, rewards]) => {
       this.log.debug('History of All Farm buybacks loaded ', hardWorks);
 
       const chartBuilder = new ChartBuilder();
-      chartBuilder.initVariables(3);
+      chartBuilder.initVariables(4);
+
+      const rewardArray = [];
+      let rewardsSum = 0;
+      rewards?.forEach(dto=>{
+        if(dto.isWeeklyReward){
+          rewardArray.push(dto.reward)
+          rewardArray.map(d=>rewardsSum+=d)
+          chartBuilder.addInData(2, dto.blockDate, rewardsSum);
+        }
+      })
 
       hardWorks?.forEach(dto => {
-        const rewardEmissionRatio = this.calcRewardComparison(dto.vaultAddress, dto.network, dto.farmBuyback);
         let bb = dto.farmBuybackSum / 1000;
         if (dto.network === 'bsc') {
           const farmPrice = this.priceData.getLastFarmPrice();
@@ -62,9 +61,11 @@ export class FarmBuybacksDialogComponent extends ChartGeneralMethodsComponent im
           } else {
             bb = 0;
           }
-        }
+      }
+        let rewardsBBRatio = bb/rewardsSum *100;
+
+        chartBuilder.addInData(3, dto.blockDate, rewardsBBRatio)
         chartBuilder.addInData(0, dto.blockDate, bb);
-        chartBuilder.addInData(2, dto.blockDate, rewardEmissionRatio);
       });
 
       this.log.debug('History of PS TVL loaded ', vaultData);
@@ -73,7 +74,8 @@ export class FarmBuybacksDialogComponent extends ChartGeneralMethodsComponent im
       this.handleData(chartBuilder, [
         ['FARM Buyback K', 'right', '#0085ff'],
         ['All supply K', '1', '#efa4a4'],
-        ['Comparison K', '2', '#28a69a']
+        ['Rewards K', '2', '#28a69a'],
+        ['Rewards to BB %', '3', 'red']
       ]);
     });
   }
