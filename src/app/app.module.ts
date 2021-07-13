e9f1f17b776c8048583c7ffcf712f5796844c0d3
Dmitry Kotelevsky <kotelevsky.dima@gmail.com>
2021-07-13 15:48:42 +0300

    test thegraph origin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {LoggerModule, NgxLoggerLevel} from 'ngx-logger';
-import {AppRoutingModule} from './app-routing.module';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {LayoutModule} from '@layout/layout.module';
-import {SidebarModule} from 'ng-sidebar';
-import {SnackBarModule} from '@shared/snack-bar/snack-bar.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CoreModule} from '@core/core.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { AppRoutingModule } from './app-routing.module';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { LayoutModule } from '@layout/layout.module';
+import { SidebarModule } from 'ng-sidebar';
+import { SnackBarModule } from '@shared/snack-bar/snack-bar.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '@core/core.module';
 
 @NgModule({
   declarations: [
