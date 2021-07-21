a7e6b4b43d5c678535139a9c4dcbb061322566b4
Dmitry Kotelevsky <kotelevsky.dima@gmail.com>
2021-07-21 19:10:54 +0300

    added percent stacked area chart, added improved version of echart approach

--- src/app/modules/dialogs/weekly-profit-dialog/weekly-profit-dialog.component.ts
+++ src/app/modules/dialogs/weekly-profit-dialog/weekly-profit-dialog.component.ts
@@ -5,6 +5,7 @@ import {ChartBuilder} from '../../chart/chart-builder';
 import {ChartGeneralMethodsComponent} from '@modules/chart/chart-general-methods.component';
 import {HardworksService} from '@data/services/http/hardworks.service';
 import {StaticValues} from '@data/static/static-values';
+declare const anychart: any;
 
 @Component({
   selector: 'app-weekly-profit-dialog',
@@ -108,8 +109,8 @@ export class WeeklyProfitDialogComponent extends ChartGeneralMethodsComponent im
         ['Weekly Profit K$', 'right', '#0085ff'],
         ['All profit M$', '1', '#eeb000']
       ]);
-      this.loadSecondChart(data);
-
+      //this.loadSecondChart(data);
+      this.loadThirdChart(data);
     });
   }
 
@@ -127,15 +128,14 @@ export class WeeklyProfitDialogComponent extends ChartGeneralMethodsComponent im
       return platform;
   }
 
-  getCurrentDate(item): string {
-      const date = new Date(item.blockDate * 1000);
+  getCurrentDate(date): string {
       const currentDate = [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('/');
       return currentDate;
   }
 
   loadSecondChart(data): void {
-      const times: Array<string> = data.map(item => this.getCurrentDate(item));
-
+      let times: Array<string> = data.map(item => this.getCurrentDate(new Date(item.blockDate * 1000)));
+      times = [...new Set(times)];
       this.option.xAxis[0].data = times;
       const groups = data.reduce((acc, item) => {
           const vaultName = item.vault;
@@ -152,10 +152,17 @@ export class WeeklyProfitDialogComponent extends ChartGeneralMethodsComponent im
       this.option.series = [];
       for (const key in groups) {
           if (groups.hasOwnProperty(key)) {
+              groups[key] = [...groups[key]].map(item => {
+                  const date = new Date(item.blockDate * 1000);
+                  item.blockDate = this.getCurrentDate(date);
+                  return item;
+              });
+              groups[key] = this.dataReducer(groups[key]);
               this.option.series.push({
                   name: key,
                   type: 'line',
                   areaStyle: {},
+                  stack: 'stack',
                   emphasis: {
                       focus: 'series'
                   },
@@ -166,7 +173,7 @@ export class WeeklyProfitDialogComponent extends ChartGeneralMethodsComponent im
               for (const i of times) {
                   let value = 0;
                   for (const j of groups[key]) {
-                      const currentDate = this.getCurrentDate(j);
+                      const currentDate = j.blockDate;
                       if (i === currentDate) {
                           value = j.fullRewardUsdTotal / 1000;
                       }
@@ -178,5 +185,140 @@ export class WeeklyProfitDialogComponent extends ChartGeneralMethodsComponent im
               seriesCounter = seriesCounter + 1;
           }
       }
+  }
+
+  dataReducer(data): any {
+        let tempArray = [...data].reverse();
+        tempArray = tempArray.filter((thing, index, self) =>
+            index === self.findIndex((t) => (t.blockDate === thing.blockDate))
+        );
+        return tempArray.reverse();
+  }
+
+  loadThirdChart(data): void {
+      const them = this.vt.getThemeColor();
+      const series = [];
+      let times: Array<string> = data.map(item => this.getCurrentDate(new Date(item.blockDate * 1000)));
+      times = [...new Set(times)];
+      this.option.xAxis[0].data = times;
+      const groups = data.reduce((acc, item) => {
+          const vaultName = item.vault;
+          const platform = this.getPlatformFromVaultName(vaultName);
+          if (!acc[platform]) {
+              acc[platform] = [];
+          }
+          acc[platform].push(item);
+          return acc;
+      }, {});
+
+      let seriesCounter = 0;
+      for (const key in groups) {
+          if (groups.hasOwnProperty(key)) {
+              groups[key] = [...groups[key]].map(item => {
+                  const date = new Date(item.blockDate * 1000);
+                  item.blockDate = this.getCurrentDate(date);
+                  return item;
+              });
+              groups[key] = this.dataReducer(groups[key]);
+              series.push({
+                  group: key,
+                  data: []
+              });
+
+              const newDataList = [];
+
+              for (const i of times) {
+                  let value = 0;
+                  for (const j of groups[key]) {
+                      const currentDate = j.blockDate;
+                      if (i === currentDate) {
+                          value = j.fullRewardUsdTotal / 1000;
+                      }
+                  }
+                  newDataList.push(value);
+              }
+              series[seriesCounter].data = newDataList;
+              seriesCounter = seriesCounter + 1;
+          }
+      }
+
+      const newDataset = [];
+      let index = 0;
+      for (const i of times) {
+          newDataset.push([]);
+          newDataset[index].push(i);
+          for (const j of series) {
+              newDataset[index].push(j.data[index]);
+          }
+          index = index + 1;
+      }
+
+
+      anychart.onDocumentReady(() => {
+          const dataSet = anychart.data.set(newDataset);
+          const seriesData = [];
+          for (let i = 0; i < newDataset[0].length; i++) {
+              seriesData.push(dataSet.mapAs({x: 0, value: i + 1}));
+          }
+
+
+          // create bar chart
+          const chart = anychart.area();
+
+          // turn on chart animation
+          chart.animation(true);
+
+          // force chart to stack values by Y scale.
+          chart.yScale().stackMode('percent');
+
+          // turn on the crosshair
+          chart.crosshair().enabled(true).yLabel().enabled(false);
+          chart.crosshair().enabled(true).xLabel().enabled(false);
+          chart.crosshair().yStroke(null).xStroke('#fff').zIndex(99);
+
+          // set yAxis labels formatting, force it to add % to values
+          chart.yAxis(0).labels().format('{%Value}%');
+
+          // helper function to setup label settings for all series
+          const setupSeries = (ser, name) => {
+              ser.stroke('3 #fff 1');
+              ser.fill(function(): string {
+                  return this.sourceColor + ' 0.8';
+              });
+              ser.name(name);
+              ser.markers().zIndex(100);
+              ser.clip(false);
+              ser.hovered()
+                  .stroke('3 #fff 1')
+                  .markers().enabled(true).type('circle').size(4).stroke('1.5 #fff');
+
+          };
+
+          // temp constiable to store series instance
+          let groupNumber = 0;
+          for (const key in groups) {
+              if (groups.hasOwnProperty(key)) {
+                  const ser = chart.area(seriesData[groupNumber]);
+                  setupSeries(ser, key);
+                  groupNumber = groupNumber + 1;
+              }
+          }
+
+          // set interactivity and toolitp settings
+          chart.interactivity().hoverMode('by-x');
+          chart.tooltip().displayMode('union');
+          // turn on legend
+          chart.legend().enabled(true).fontSize(13).padding([0, 0, 25, 0]);
+          let themColor = '#fffce6 0.2';
+          if (them === 'dark') {
+             themColor = '#404040 0.2';
+          }
+          chart.background({fill: themColor});
+          // set container id for the chart
+          chart.container('anychart');
+
+          // initiate chart drawing
+          chart.draw();
+      });
   }
 }
