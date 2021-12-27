0878e80553abb7651501bb30e7f2d661c5f655c5
Maksim Goncharov <magoncharov@tsum.com>
2021-12-27 20:45:15 +0300

    Generate angular env from node env

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-// DON'T USE THIS FOR RUNTIME VARIABLES
-// 1) make changes to src/assets/config.[environment].json
-// 2) update src/app.config.ts
-// 3) add constructor(@Inject(APP_CONFIG) public config: AppConfig) to your class
-// 4) access config via this.config.apiEndpoint
-
-export const environment = {
-  production: true,
-};
