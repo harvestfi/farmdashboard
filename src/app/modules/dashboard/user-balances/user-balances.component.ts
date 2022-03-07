562ec0f2d5204d0bf934b72871f533d3ef88ebe1
Maksim Goncharov <magoncharov@tsum.com>
2022-03-08 00:46:51 +0300

    Improve and fix styles and layouts in “Users Balances” page

--- src/app/modules/dashboard/user-balances/user-balances.component.ts
+++ src/app/modules/dashboard/user-balances/user-balances.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { HttpService } from '@data/services/http/http.service';
 import { NGXLogger } from 'ngx-logger';
 import { Balance } from '@data/models/balance';
@@ -11,38 +11,40 @@ import { takeUntil } from 'rxjs/operators';
   selector: 'app-user-balances',
   templateUrl: './user-balances.component.html',
   styleUrls: ['./user-balances.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [DestroyService],
 })
 export class UserBalancesComponent implements AfterViewInit {
   @Input('data') public data;
-  userBalances: Balance[];
+  
+  public userBalances: Balance[];
+  public ethNetworkScan: string = Utils.getNetworkScanUrl('eth');
   public vt: ViewTypeService = new ViewTypeService();
-
+  
   constructor(
     private httpService: HttpService,
     private log: NGXLogger,
     private destroy$: DestroyService,
+    private changeDetectorRef: ChangeDetectorRef,
   ) {
   }
-
+  
   ngAfterViewInit(): void {
     this.loadData();
   }
-
-
+  
+  
   private loadData(): void {
     this.httpService.getUserBalances()
       .pipe(takeUntil(this.destroy$))
       .subscribe(data => {
         this.userBalances = data.filter(b => !isNaN(+b.balance) && b.balance !== Infinity);
+        
+        this.changeDetectorRef.detectChanges()
       });
   }
-
+  
   shortAddress(address: string): string {
     return address.slice(0, 5) + '...' + address.slice(address.length - 4);
-  }
-
-  getNetworkScan(): string {
-    return Utils.getNetworkScanUrl('eth');
   }
 }
