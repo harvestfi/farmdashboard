9e7e88856d90b7ffc7acc5d9c7acae1485d998d9
alex <alexandersazonov0@gmail.com>
2022-04-25 23:42:22 +0300

    Fix problem

--- server.js
+++ server.js
@@ -14,46 +14,43 @@ app.use(cors());
 app.use(express.static(__dirname + '/dist/farmdashboard-front'));
 
 app.get('/harvestFinance/vaults', function(req, res) {
-    try {
-        https.get('https://api-ui.harvest.finance/vaults?key=41e90ced-d559-4433-b390-af424fdc76d6', (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                console.log('data: ' + data);
-                try {
-                    return res.send(JSON.parse(data));
-                } catch (e) {
-                    console.log('Error: ' + e);
-                    return res.send("{}");
-                }
-            });
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+    https.get('https://api-ui.harvest.finance/vaults?key=41e90ced-d559-4433-b390-af424fdc76d6', (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
-    } catch (e) {
-        console.log('Error: ' + e);
-    }
+        resp.on('end', () => {
+            console.log('data: ' + data);
+            try {
+                return res.send(JSON.parse(data));
+            } catch (e) {
+                console.log('Error: ' + e);
+                return res.send("{}");
+            }
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
 
 });
 
 app.get('/matic-info', function(req, res) {
-    try {
-        https.get('https://api.coingecko.com/api/v3/simple/price?include_last_updated_at=true&ids=matic-network&vs_currencies=usd', (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                return res.send(JSON.parse(data)["matic-network"]);
-            });
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+    https.get('https://api.coingecko.com/api/v3/simple/price?include_last_updated_at=true&ids=matic-network&vs_currencies=usd', (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
-    } catch (e) {
-        console.log('Error: ' + e);
-    }
+        resp.on('end', () => {
+            try {
+                return res.send(JSON.parse(data)["matic-network"]);
+            } catch (e) {
+                console.log('Error: ' + e);
+                return res.send("{}");
+            }
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
 
 });
 
